9f4bd11c7e6175147276dbebbcc76a559466f27a
Marcos Vargens <mvargens@gmail.com>
2022-07-17 05:28:57 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import NotificationButton from "./components/NotificationButton";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import SalesCard from "./components/SalesCard";
-
 import Header from "./components/Header";
+
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
